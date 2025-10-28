9dee6377103195a9d65c318a6c5bf24c621bae11
Tushar-designs-arc <sainitushar322@gmail.com>
2025-10-28 11:08:42 +0530

    first commit

--- Star_Patterns.cpp
+++ Star_Patterns.cpp
@@ -8,9 +8,9 @@ int main() {
     cout << "Enter the no. of rows: ";
     cin >> n;
 
-    for (int i = 1; i <= n; ++i) {
+    for (int i = 0; i < n; i++) {
 
-        for (int j = 0; j < n; ++j) {
+        for (int j = 0; j < n; j++) {
             cout << "* ";
         }
 
@@ -33,10 +33,14 @@ int main() {
     cout << "Enter the no. of rows: ";
     cin >> n;
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
 
-        for (int j = 0; j < i; j++) {
+        for (int j = 0; j <= i; j++) {
             cout << "* ";
+        }
+        
+        for (int k = (n - i); k > 0; k--) { // k = 0; k < (n - i); k++, (if we set k = 1, or give condition k > 1 the extra line of spaces will not be printed.)
+            cout << "  ";
         }
         
         cout << endl;
