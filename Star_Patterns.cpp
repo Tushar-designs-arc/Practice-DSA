1083f3b25b9ac8f2cbbf68edff2357bba67626ca
Tushar-designs-arc <sainitushar322@gmail.com>
2025-10-30 11:48:25 +0530

    second commit

--- Star_Patterns.cpp
+++ Star_Patterns.cpp
@@ -39,13 +39,42 @@ int main() {
             cout << "* ";
         }
         
-        for (int k = (n - i); k > 0; k--) { // k = 0; k < (n - i); k++, (if we set k = 1, or give condition k > 1 the extra line of spaces will not be printed.)
+        for (int k = (n - i); k > 0; k--) { // Or, k = 0; k < (n - i); k++, (if condition k > 1, or k = 1. then, the extra line of spaces will not be printed.)
             cout << "  ";
         }
         
         cout << endl;
     }
     
+    return 0;
+}
+
+/* ------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+
+/* Reverse Right Half Pyramid */
+
+#include <iostream>
+using namespace std;
+    
+int main() {
+    int n;
+    cout << "Enter the no. of rows: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        
+        for (int j = (n - i); j > 0; j--) { // Or, j = 0; j < (n - i); j++
+            cout << "* ";
+        }
+
+        for (int k = 0; k <= i; k++) { // if condition k < i, ya k = 1; k <= i. then, the extra line of spaces will not be printed.
+            cout << "  ";
+        }
+        
+        cout << endl;
+    }
+
     return 0;
 }
 
