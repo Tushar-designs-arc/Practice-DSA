ddc283d1fc52034e980690261803625d7776c6a6
Tushar <sainitushar322@gmail.com>
2025-10-31 19:58:33 +0530

    third commit

--- Star_Patterns.cpp
+++ Star_Patterns.cpp
@@ -108,3 +108,32 @@ int main() {
 }
 
 /* ------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+
+/* Reverse Left Half Pyramid */
+
+#include <iostream>
+using namespace std;
+    
+int main() {
+    int n;
+    cout << "Enter the no. of rows: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        
+        for (int j = 0; j <= i; j++) { // if j = 1, or condition j < i. then, the extra line of spaces will not be printed.
+            cout << "  ";
+        }
+
+        for (int k = (n - i); k > 0; k--) { // Or, k = 0; k < (n - i); k++
+            cout << "* ";
+        }
+        
+        cout << endl;
+    }
+    
+    return 0;
+}
+
+/* ------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
